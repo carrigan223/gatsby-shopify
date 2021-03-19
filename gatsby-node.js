47381aa34724a1c0ad1abde678552065e75300c7
Andrew Carrigan <carrigan223@gmail.com>
2021-03-18 17:09:20 -0700

    added image gallery

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,35 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     },
   });
 };
+
+//create pages is a reserved name for a funtction
+//it is informing gatsby to inject helpers on page creation
+//graphql is allowing data query
+//actions is accesing the redux actions under the hood
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const { data } = await graphql(`
+    {
+      allShopifyProduct {
+        edges {
+          node {
+            shopifyId
+            handle
+          }
+        }
+      }
+    }
+  `);
+  //we are taking in our shopify products and creating individual pages based
+  //upon a template
+  data.allShopifyProduct.edges.forEach(({node}) => {
+    createPage({
+      path: `products/${node.handle}`,
+      context: {
+        shopifyId: node.shopifyId
+      },
+      component: path.resolve('./src/templates/ProductTemplates/index.js')
+    })
+  });
+};
---
+++ src/templates/ProductTemplates/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Grid = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  padding-top: 20px;
+  //setting a media query for desktop to display multiple columns
+  @media (min-width: 760px) {
+    grid-template-columns: 1fr 1fr; //two columns of equal width
+
+    //flipping the order to be displayed on larger views(last div first, first div last)
+    > div:first-child {
+      order: 2;
+    }
+
+    > div:last-child {
+      order: 1;
+    }
+  }
+`;

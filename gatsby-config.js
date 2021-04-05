836810688bc23e86d7c98f045df2a228d3967f69
Andrew Carrigan <carrigan223@gmail.com>
2021-04-04 20:43:09 -0700

    last time

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,20 @@ module.exports = {
     siteUrl: `https://thesharpestcollectibles.com/`,
   },
   plugins: [
-    `gatsby-plugin-advanced-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `The Sharpest Collectibles`,
+        short_name: `The Sharpest`,
+        start_url: `/`,
+        background_color: `#002D72`,
+        theme_color: `#FF5910`,
+        display: `standalone`,
+        icon: `src/images/PWAicon.png`,
+      },
+    },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-robots-txt`,
@@ -40,17 +52,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-shopify-starter`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-      },
-    },
   ],
 };

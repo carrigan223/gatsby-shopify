fcec796772407ab35c75f6cef5aaad4e0eed23cb
Andrew Carrigan <carrigan223@gmail.com>
2021-04-04 19:39:53 -0700

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,17 +63,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-shopify-starter`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-      },
-    },
   ],
 };

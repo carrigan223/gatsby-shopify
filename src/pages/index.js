085a4d1bb4a8a32a3f75262c9a5bf0a690e92bb7
Andrew Carrigan <carrigan223@gmail.com>
2021-04-15 13:32:57 -0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+// import { Link } from 'gatsby';
 import {
   Layout,
   HomePageCollectionsGrid,
@@ -10,6 +11,31 @@ import ProductContext from '../context/ProductContext';
 import styled from 'styled-components';
 
 const StyledSection = styled.div``;
+
+// const HeaderContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   flex-direction: column;
+// `;
+
+// const FreeShipping = styled.h2`
+//   text-align: center;
+//   margin-bottom: 4rem;
+//   font-size: 4rem;
+//   font-family: 'Permanent Marker', 'Courier New', Courier, monospace;
+//   color: rgb(0, 45, 114);
+//   text-shadow: 2px 2px 8px grey;
+// `;
+
+// const Browse = styled(Link)`
+//   font-family: 'Permanent Marker', 'Courier New', Courier, monospace;
+//   color: black;
+//   text-decoration: none;
+//   padding: 1.5rem;
+//   margin-bottom: 1rem;
+//   border-radius: 50%;
+//   border: 1px solid rgb(0, 45, 114);
+// `;
 
 const IndexPage = () => {
   //bringing in collections from context
@@ -23,9 +49,14 @@ const IndexPage = () => {
           title="Homepage"
           description="The Sharpest Collectibles Homepage, Your stop for Trading Cards coins and other collectibles."
         />
+        {/* <HeaderContainer>
+          <FreeShipping>Enjoy Free Shipping On All Our Products!</FreeShipping>
+          <Browse to="/all-products">Browse Now</Browse>
+        </HeaderContainer> */}
+
         <HomePageCollectionsGrid
-          collections={collections.filter(collection =>
-            !collection.title.includes('Featured')
+          collections={collections.filter(
+            collection => !collection.title.includes('Featured')
           )}
         />
         {/* the above code is filtering out the Featured collection for our collection categories
